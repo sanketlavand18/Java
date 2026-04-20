474871d4c04fbe2e883bb64f5b6926f8d767b801
Sanket Lavand 18 <sanketlavand1824@gmail.com>
2026-04-20 15:40:51 +0530

    Add files via upload

---
+++ Self_Learning/src/Assignment_1/Q3_StringMethods.java
@@ -0,0 +1,33 @@
+package Assignment_1;
+
+public class Q3_StringMethods {
+
+	public static void main(String[] args) {
+
+		String st = "Welcome to Java";
+		String s = "welcome to java";
+
+		System.out.println(st);
+		System.out.println(s);
+		System.out.println(".....Methods....");
+
+		System.out.println("lenght(): " + st.length());
+		System.out.println("Uppeecase(): " + st.toUpperCase());
+		System.out.println("Lowercase(): " + st.toLowerCase());
+		System.out.println("equals(): " + st.equals(s));
+		System.out.println("equalsignorecase(): " + st.equalsIgnoreCase(s));
+		System.out.println("contains(): " + st.contains("to"));
+		System.out.println("indexof(): " + st.indexOf('o'));
+		System.out.println("lastindexof(): " + st.lastIndexOf('o'));
+		System.out.println("substring(): " + st.substring(4));
+		System.out.println("Replace(): " + st.replace('a', 'p'));
+		System.out.println("Trim(): " + st.trim());
+		System.out.println("concat(): " + st.concat(s));
+		System.out.println("charat(): " + st.charAt(3));
+		System.out.println("is empty(): " + st.isEmpty());
+		System.out.println("Startwith(): " + st.startsWith("welcome"));
+		System.out.println("endwith(): " + st.endsWith("java"));
+
+	}
+
+}
